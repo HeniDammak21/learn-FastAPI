1bee118c951828a6593ddd6d5c9678171561cb8c
Heni <henidammak@gmail.com>
2025-04-29 17:03:40 +0100

    finish application with adding router and auth

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,5 +1,44 @@
 from pydantic import BaseModel
+from typing import List
+
 
 class Blog(BaseModel):
-    title:str
-    body:str
+    title: str
+    body: str
+
+class User(BaseModel):
+    name:str
+    email:str
+    password:str
+
+class ShowUser(BaseModel):
+    name:str
+    email:str
+    blogs: List[Blog] =[]
+    class Config():
+        orm_mode = True 
+
+class ShowBlog(BaseModel):
+    title: str 
+    body: str 
+    creator:ShowUser
+    class Config():
+        orm_mode = True
+
+class login(BaseModel):
+    username:str
+    password:str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: str | None = None
+
+
+    
+
+
+
